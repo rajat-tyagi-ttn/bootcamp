3401e99224224b32c59d0b2309f8154ba8a5c44a
rajattyagi-ttn <60641678+rajattyagi-ttn@users.noreply.github.com>
2020-02-23 23:21:18 +0530

    Playground File Added

---
+++ Intermediate.playground/Contents.swift
@@ -0,0 +1,120 @@
+import UIKit
+
+//var str = "Hello, playground"
+
+func siftBeans(fromGroceryList : [String]) -> (beans : [String], otherGroceries : [String]){
+    var beans = [String]()
+    var otherGroceries = [String]()
+    
+    for elem in fromGroceryList{
+        if elem.hasSuffix("beans"){
+            beans.append(elem)
+        }
+        else{
+            otherGroceries.append(elem)
+        }
+    }
+    return (beans, otherGroceries)
+}
+
+let result = siftBeans(fromGroceryList: ["green beans",
+
+                                        "milk",
+
+                                        "black beans",
+
+                                        "pinto beans",
+
+                                        "apples"])
+
+result.beans
+result.otherGroceries
+
+
+enum Operations{
+    case add(Double,Double)
+    case subtract(Double,Double)
+    case multiply(Double,Double)
+    case divide(Double,Double)
+    case squareroot(Double)
+}
+
+class Calculator{
+    
+    func equals(_ operation: Operations ) -> Double{
+        
+        switch operation{
+            
+            case .add(let num1, let num2):
+                return num1 + num2
+                
+            case .subtract(let num1, let num2):
+                return num1 - num2
+                
+            case .multiply(let num1, let num2):
+                return num1 * num2
+            
+            case .divide(let num1, let num2):
+                return num1/num2
+            
+            case .squareroot(let num1):
+                return num1.squareRoot()
+        }
+    }
+}
+    
+var c = Calculator()
+c.equals(Operations.add(2, 3))
+c.equals(Operations.subtract(7, 3))
+c.equals(Operations.multiply(45, 3))
+c.equals(Operations.divide(75, 3))
+c.equals(Operations.squareroot(81))
+
+
+var addClosure : ((Double,Double?) -> Double) = {(_ num1 : Double, _ num2 : Double?) -> Double in
+    return num1 + num2!
+}
+
+var subtractClosure : ((Double,Double?) -> Double) = {(_ num1 : Double, _ num2 : Double?) -> Double in
+    return num1 - num2!
+}
+
+var multiplyClosure : ((Double,Double?) -> Double) = {(_ num1 : Double, _ num2 : Double?) -> Double in
+    return num1 * num2!
+}
+
+var divideClosure : ((Double,Double?) -> Double) = {(_ num1 : Double, _ num2 : Double?) -> Double in
+    return num1 / num2!
+}
+
+var squareRootClosure : ((Double,Double?) -> Double) = {(_ num1 : Double,_ num2 : Double?) -> Double in
+    return num1.squareRoot()
+}
+
+ 
+class Calc{
+    
+    func equal(_ number1 : Double, _ number2 : Double?, _ oper : (Double,Double?) -> Double) -> Double{
+        if number2 != nil{
+            return oper(number1,number2!)
+        }
+        else{
+            return oper(number1,nil)
+        }
+        
+    }
+}
+
+var ca = Calc()
+
+ca.equal(2, 6, addClosure)
+ca.equal(14, 7, subtractClosure)
+ca.equal(3, 50, multiplyClosure)
+ca.equal(12, 3, divideClosure)
+ca.equal(81, nil, squareRootClosure)
+
+
+
+
+
+
