0760d2c7306b69f8058db066497a085328df29ae
rajattyagi-ttn <60641678+rajattyagi-ttn@users.noreply.github.com>
2020-06-05 02:23:31 +0530

    Gallery App Through API Integration & JSON serialization

---
+++ RajatBootcamp/RajatBootcamp/GalleryApplication/ImageCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCollectionViewCell.swift
+//  RajatBootcamp
+//
+//  Created by Rajat Tyagi on 04/06/20.
+//  Copyright © 2020 Rajat Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var cellImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
